0e1c6f5dae8a0e88ab3e619f3954b02fc4f9c6eb
Jakub Brzegowski <symfiz@gmail.com>
2016-03-21 00:41:58 +0100

    Implemented simple program displaying CPUID info now using printf function. Compiled with linker and gcc. Modified Makefile.

---
+++ examples_from_book/cpuid_printf.s
@@ -0,0 +1,31 @@
+# Source: Professional Assembly Language by Richard Blum
+# cpuid_printf.s View the CPUID Vendor ID string using C library calls
+
+.section .data
+
+output:
+	.asciz "The processor Vendor ID is '%s'\n"
+
+
+.section .bss
+	.lcomm buffer, 12
+
+
+.section .text
+.global _start 
+
+_start:
+	movq	$0, %rax
+	cpuid
+	movq	$buffer, %rdi
+	movq	%rbx, (%rdi)
+	movq	%rdx, 4(%rdi)
+	movq	%rcx, 8(%rdi)
+	movq	$buffer, %rsi
+	movq	$output, %rdi
+	movq	$0, %rax
+	call	printf
+	addq	$8, %rsp
+	pushq	$0
+	call	exit
+	
